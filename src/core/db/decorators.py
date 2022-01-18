13e74f0f5bf83b2bc258829edd3caf260449fd48
arkhipov <arhipov@technokratos.com>
2022-01-18 23:54:53 +0300

    fix circular import

--- src/core/db/decorators.py
+++ src/core/db/decorators.py
@@ -1,32 +1,7 @@
 import functools
 import inspect
-from contextlib import asynccontextmanager
-from typing import AsyncContextManager, Callable
-
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.db.session import transactional_async_scoped_session_factory
-
-
-def transactional_async_session_maker(
-    async_session_factory_: Callable[..., AsyncSession]
-) -> Callable[..., AsyncContextManager[AsyncSession]]:
-    @asynccontextmanager
-    async def wrapper(**kwargs) -> AsyncContextManager[AsyncSession]:
-        """Provide a transactional scope around a series of operations."""
-        db = async_session_factory_(**kwargs)
-
-        try:
-            yield db
-        except Exception:
-            await db.rollback()
-            raise
-        else:
-            await db.commit()
-        finally:
-            await db.close()
-
-    return wrapper
 
 
 def transactional(func):
--- src/core/db/session.py
+++ src/core/db/session.py
@@ -1,4 +1,6 @@
 from asyncio import current_task
+from contextlib import asynccontextmanager
+from typing import AsyncContextManager, Callable
 
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
@@ -7,13 +9,36 @@ from sqlalchemy.ext.asyncio import (
 )
 from sqlalchemy.orm import sessionmaker
 
-from src.core.db.decorators import transactional_async_session_maker
 from src.core.settings import postgres_settings
 
+
+def _transactional_async_session_maker(
+        async_session_factory_: Callable[..., AsyncSession]
+) -> Callable[..., AsyncContextManager[AsyncSession]]:
+    @asynccontextmanager
+    async def wrapper(**kwargs) -> AsyncContextManager[AsyncSession]:
+        """Provide a transactional scope around a series of operations."""
+        db = async_session_factory_(**kwargs)
+
+        try:
+            yield db
+        except Exception:
+            await db.rollback()
+            raise
+        else:
+            await db.commit()
+        finally:
+            await db.close()
+
+    return wrapper
+
+
 async_engine = create_async_engine(postgres_settings.async_url)
+
 async_session_factory = sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 async_scoped_session_factory = async_scoped_session(async_session_factory, scopefunc=current_task)
-transactional_async_session_factory = transactional_async_session_maker(async_session_factory)
-transactional_async_scoped_session_factory = transactional_async_session_maker(
+
+transactional_async_session_factory = _transactional_async_session_maker(async_session_factory)
+transactional_async_scoped_session_factory = _transactional_async_session_maker(
     async_scoped_session_factory
 )
